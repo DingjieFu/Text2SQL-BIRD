b38091fd85cf713321309a2fa2e08a9eb47d17b3
Dingjie Fu <66894525+DingjieFu@users.noreply.github.com>
2025-08-01 10:20:55 +0800

    Create __init__.py

---
+++ src/cscsql/__init__.py
@@ -0,0 +1,7 @@
+"""
+    - 2025.7.24
+    - CSC-SQL: effective SQL post-processing approach [https://github.com/CycloneBoy/csc_sql]
+    - utils.py -> obtain input data, NL2SQL metrics, ...
+    - exec_func.py -> SQL execution functions
+    - selection.py -> voting 
+"""
