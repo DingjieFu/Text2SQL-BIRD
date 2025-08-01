65dd92b15aee13779397dc684ea14cf16ce603e6
Dingjie Fu <66894525+DingjieFu@users.noreply.github.com>
2025-08-01 10:16:38 +0800

    Create __init__.py

---
+++ src/mschema/__init__.py
@@ -0,0 +1,6 @@
+"""
+    - 2025.7.24
+    - schema_engine.py && m_schema.py -> codes for extract MSchema for database [https://github.com/XGenerationLab/M-Schema]
+    - extract_schema.py -> extract and save database schema (DDL-Schema and M-Schema)
+    - utils.py -> file writing and reading functions
+"""
